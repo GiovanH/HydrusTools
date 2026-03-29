becba32fd172064516ecfce5a4f650f6ad41b640
Gio <GiovanH@users.noreply.github.com>
2026-03-28 19:16:56 -0500

    Macro rules!

--- hydrustools/macro/macro_rules.py
+++ hydrustools/macro/macro_rules.py
@@ -1,23 +1,31 @@
 import logging
+from dataclasses import dataclass
 
 import hydrus_api
+from hydrus_api import AndQuery, Literal
 
 from hydrustools.settings import HTSettings, settings_section
-from hydrustools.utils import htlogging, querylang
+from hydrustools.utils import htlogging
 
 from ..utils import hydrus
 
 logger = logging.getLogger(__name__)
 
+@dataclass
+class QueryAction():
+    query: AndQuery
+    reason: str | None = None
+    confirm: bool | None = None
+    add_tags: list[str] | None = None
+    remove_tags: list[str] | None = None
+    image_action: Literal['DELETE', 'FAVORITE', 'INBOX', 'ARCHIVE'] | None = None
+
 @settings_section(section="MacroRules")
 class Settings(HTSettings):
     tags_delete: list[str] = []
     tags_flatten_to_parents: list[str] = []
-    flatten_presearch_hl: list[str] = []
-    flatten_search: str = ""
-    flatten_search_hl: list[str] = []
-    delete_query_matches: dict[str, list] = {}
-
+    # delete_query_matches: dict[str, list] = {}
+    query_actions: list[QueryAction] = []
 
 def disambiguate_chars_in_series(series: str, characters: list[str]):
     for character in characters:
@@ -28,28 +36,69 @@ def disambiguate_chars_in_series(series: str, characters: list[str]):
             [char_tag, f'series:{series}']
         )
 
+def maybe_confirm(qa: QueryAction) -> bool:
+    if qa.confirm:
+        resp = input("Continue? Y/N > ")
+        return resp.lower() == 'y'
+    return True
+
+
+def apply_query_action(qa: QueryAction):
+    print(qa)
+
+    file_ids = hydrus.client.search_files(
+        tags=qa.query,
+        tag_service_key=hydrus.local_tags_service_key
+    )['file_ids']
+
+    msg = f"Query {qa.query} matcheed {len(file_ids)} images"
+    if len(file_ids) < 1:
+        logger.debug(msg)
+        return
+
+    logger.info(msg)
+
+    if qa.add_tags or qa.remove_tags:
+        add_tags = qa.add_tags or []
+        remove_tags = qa.remove_tags or []
+
+        hydrus.add_implying_siblings(remove_tags)
+
+        logger.info(f"Adding {add_tags} and removing {remove_tags} from {len(file_ids)} matching images")
+        if not maybe_confirm(qa): return
+
+        hydrus.client.add_tags(
+            file_ids=file_ids,
+            service_keys_to_actions_to_tags={
+                hydrus.local_tags_service_key: {
+                    hydrus_api.TagAction.ADD: add_tags,
+                    hydrus_api.TagAction.DELETE: remove_tags
+                }
+            }
+        )
+
+    if qa.image_action == 'DELETE':
+        if not qa.reason:
+            raise ValueError("A reason is required to delete images")
+
+        if qa.confirm is None:
+            qa.confirm = True
+
+        logger.info(f"Deleting {len(file_ids)} images")
+        if not maybe_confirm(qa): return
+        hydrus.client.delete_files(
+            file_ids=file_ids,
+            reason=qa.reason
+        )
+    if qa.image_action == 'FAVORITE':
+        raise NotImplementedError()
+    if qa.image_action == 'INBOX':
+        raise NotImplementedError()
+    if qa.image_action == 'ARCHIVE':
+        raise NotImplementedError()
+
 
 def run(tk=True):
-    for tag_name in Settings.tags_delete:
-        logger.info(f"Deleting tag {tag_name}")
-        try:
-            hydrus.replace_tag(tag_name, [])
-        except hydrus_api.MissingParameter:
-            logger.info("Nothing to do!")
-
-    hydrus.remove_tags_from_matches(
-        ['meta:bad tag'],
-        ['meta:bad tag']
-    )
-
-    hydrus.remove_tags_from_matches(
-        ['creator:*', 'todo:artist'],
-        ['todo:artist']
-    )
-    hydrus.remove_tags_from_matches(
-        ['series:*', 'todo:series'],
-        ['todo:series']
-    )
 
     for source_tag in Settings.tags_flatten_to_parents:
         hydrus.flatten_tag_to_parents(source_tag)
@@ -59,11 +108,21 @@ def run(tk=True):
         ['alex', 'clover', 'sam']
     )
 
-    for reason, query in Settings.delete_query_matches.items():
-        logger.info(f"Deleting images matching query {query} for reason {reason}")
-        hydrus.delete_all_query_matches(
-            reason=reason, query=query
+    query_actions = [*Settings.query_actions]
+
+    for tag_name in Settings.tags_delete:
+        query_actions.append(
+            QueryAction(
+                query=[tag_name],
+                remove_tags=[tag_name]
+            )
         )
+
+    for qa in query_actions:
+        try:
+            apply_query_action(qa)
+        except KeyboardInterrupt:
+            break
 
 
 if __name__ == "__main__":
